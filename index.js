8e34e732011fe90a0889a2a5b71eb4940e71edbf
Cristina Murillo <cmurill1@jhu.edu>
2018-07-22 18:41:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,5 +23,7 @@ function sayHiToGrandma(string){
   if (string===lowercase){
     return "I can't hear you!"
   }
-  if
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
+  }
 }
